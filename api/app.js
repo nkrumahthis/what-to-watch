0cbb463533d28b97ba8db4f24886ae1fdcb15d2f
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-27 23:41:23 +0000

    cors

--- api/app.js
+++ api/app.js
@@ -2,29 +2,30 @@ import express from "express"
 import axios from "axios";
 import dotenv from "dotenv";
 import process from "process";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.VITE_APIPORT;
+const port = process.env.API_PORT;
 
-app.get('/movies', async (_, res) => {
+app.use(cors())
+
+app.get('/movies', async (req, res) => {
+    console.log("get movies from " + req.ip)
     try {
         const response = await axios.get(
-            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOVIEDB_KEY}&include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
+            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOVIE_DB_KEY}&include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc`,
         )
 
         return res.json(response.data)
 
     } catch (error) {
         console.error(error)
-        res.status(500).json({ error: "server error" });
+        return res.status(500).json({ error: "server error" });
     }
-
-
-    return res.json("hello world")
 });
 
 app.listen(port, () => {
-    console.log("listening on port " + port);
+    console.log("listening on http://localhost:" + port);
 });
