1edd38268fdb1ea057ab28e438e0fb76b392134f
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-28 23:50:55 +0000

    connect to mongo, vote and get votes

--- api/app.js
+++ api/app.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import dotenv from "dotenv";
 import process from "process";
 import cors from "cors";
+import mongoose from "mongoose";
+import MovieVotes from "./MovieVotes.js";
 
 dotenv.config();
 
@@ -26,6 +28,41 @@ app.get('/movies', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log("listening on http://localhost:" + port);
+app.post('/votes', async (req, res) => {
+    const movieVote = await MovieVotes.findOne({movie : req.params.movie})
+    if(!movieVote) {
+        const newMovieVote = await MovieVotes.create({
+            movie: req.params.movie,
+            votes: 1
+        })
+        return res.status(200).json({ movieVote: newMovieVote});
+    } else {
+        movieVote.votes += 1;
+        await movieVote.save()
+        res.status(200).json({ movieVote: movieVote});
+    }
 });
+
+app.get('/votes', async (req, res) => {
+    if(req.query.movie){
+        const movieVote = await MovieVotes.findOne({data: req.query.movie});
+        if(!movieVote) {
+            return res.status(404).json({error: 'Movie not found'});
+        }
+        return res.json({ data: movieVote })
+    }
+
+    const movieVotes = await MovieVotes.find({});
+    return res.json({ data: movieVotes });
+});
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connection established")
+
+    app.listen(port, () => {
+        console.log("listening on http://localhost:" + port);
+    });
+}
+
+main().catch(err => console.log(err));
