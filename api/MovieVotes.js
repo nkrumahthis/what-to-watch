95562aec711c6821d18a4f1a3454024f92f20ada
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-04-29 00:10:42 +0000

    update schema

--- api/MovieVotes.js
+++ api/MovieVotes.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
 export default mongoose.model('MovieVotes', new mongoose.Schema({
-    movie: String,
+    id: String,
+    title: String,
+    poster_path: String,
+    overview: String,
     votes: Number
 }));
