9f035ebe2079cf69352c6e033bd723f90de3b787
Nkrumah Sarpong <nkrumahthis@gmail.com>
2024-05-12 23:35:57 +0000

    results page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Root from './routes/root.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Results from './Results.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <h1>404</h1>,
+    children : [
+      {
+        path: "/",
+        element: <App />
+      },
+      {
+        path: "/results",
+        element: <Results />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
