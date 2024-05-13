ebd94690b3320a398e15055863a466dbccf8a5df
Nkrumah Sarpong <nkrumahthis@gmail.com>
2024-05-13 22:30:17 +0000

    loading

--- src/Results.jsx
+++ src/Results.jsx
@@ -6,8 +6,11 @@ export default function Results() {
   const apiUrl = `http://localhost:${import.meta.env.VITE_API_PORT}`
 
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
 
   const fetch = () => {
+    setLoading(true);
     axios
         .get(`${apiUrl}/votes`)
         .then((res) => {
@@ -18,6 +21,9 @@ export default function Results() {
         })
         .catch((err) => {
             console.error("hehe", err);
+        })
+        .finally(() =>{
+            setLoading(false);
         });
   };
 
@@ -29,18 +35,32 @@ export default function Results() {
   return (
     <div className="page">
         <h2>Results</h2>
-        <table>
-            <tbody>
-                {results ? results.map((result, index) => <tr key={index}>
-                    <td>
-                        <img
-                            src={`https://image.tmdb.org/t/p/w92${result.poster_path}`}
-                        ></img></td>
-                    <td>{ result.title }</td>
-                    <td>{result.votes}</td>
-                </tr>) : <tr><td>No results</td></tr>}
-            </tbody>
-        </table>
+        { loading ? <p>Loading...</p> : 
+            (
+                results.length ? 
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Poster</th>
+                                <th>Title</th>
+                                <th>Votes</th>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            { results.map((result, index) => 
+                                <tr key={index}>
+                                    <td><img src={`https://image.tmdb.org/t/p/w92${result.poster_path}`}/></td>
+                                    <td>{ result.title }</td>
+                                    <td>{ result.votes }</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                : 
+                    <p>No results</p>
+            )
+        }
+        
     </div>
   )
 }
